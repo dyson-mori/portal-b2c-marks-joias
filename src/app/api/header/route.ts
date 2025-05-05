762ec6caeafdfca31e0df4dba13603e4a8651130
dyson-mori <161769883+dyson-mori@users.noreply.github.com>
2025-05-05 15:05:03 -0300

    header

--- src/app/api/header/route.ts
+++ src/app/api/header/route.ts
@@ -5,8 +5,8 @@ export async function GET() {
   const header = await prisma.header.findMany();
 
   if (!header) {
-    return NextResponse.json([], { status: 400, statusText: 'products not received' });
+    return NextResponse.json([], { status: 400, statusText: 'header not received' });
   };
 
-  return NextResponse.json(header, { status: 200, statusText: 'products received successfully' });
+  return NextResponse.json(header, { status: 200, statusText: 'header received successfully' });
 };
