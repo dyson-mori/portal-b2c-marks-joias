23183d6a5eccb09f1b4a1524a7d6ed62e7fa9d22
dyson-mori <161769883+dyson-mori@users.noreply.github.com>
2025-05-05 15:53:00 -0300

    remove aside

--- src/app/products/index.tsx
+++ src/app/products/index.tsx
@@ -6,11 +6,10 @@ import { DotLottiePlayer } from '@dotlottie/react-player';
 import { useTheme } from 'styled-components';
 import { SubCategory } from '@prisma/client';
 
-import { Tag } from '@assets';
-import { Input, Product, Card } from '@components';
+import { Product } from '@components';
 import { CategoryProps, ProductProps } from "@global/interfaces";
 
-import { Container, Aside, Products as ProductsStyled, ProductEmpty } from './styles';
+import { Container, Products as ProductsStyled, ProductEmpty } from './styles';
 
 type Props = {
   products: ProductProps[];
@@ -35,19 +34,6 @@ export default function Products({ products, categories }: Props) {
 
   return (
     <Container>
-      <Aside>
-        <Input.Root>
-          <Tag width={20} height={20} stroke={theme.colors.primary} strokeWidth={2} />
-          <Input.Input placeholder='Product Name' onChange={console.log} />
-        </Input.Root>
-        <span style={{ height: 10 }} />
-        {
-          categories.map(({ title, sub }, index) =>
-            <Card key={index} title={title} icon={Tag} data={sub} selects={selects} setSelect={setSelects} />
-          )
-        }
-      </Aside>
-
       {products.filter(e => e.name.toLowerCase().includes(label.toLowerCase())).length === 0 && (
         <ProductEmpty>
           <DotLottiePlayer style={lottie_styles} src="/lottie/marks-empty-card.lottie" autoplay />
