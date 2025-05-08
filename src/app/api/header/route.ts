8f2f8badb9739f105e6f110635d323c38fa063b4
dyson-mori <161769883+dyson-mori@users.noreply.github.com>
2025-05-08 16:19:45 -0300

    create header

--- src/app/api/header/route.ts
+++ src/app/api/header/route.ts
@@ -1,20 +1,32 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+import { Header } from "@prisma/client";
+
 import { prisma } from "@services/prisma";
 
 export async function GET() {
   const header = await prisma.header.findMany();
-
-  // await prisma.header.create({
-  //   data: {
-  //     param: '/',
-  //     public: true,
-  //     title: 'Produtos'
-  //   }
-  // })
 
   if (!header) {
     return NextResponse.json([], { status: 400, statusText: 'header not received' });
   };
 
   return NextResponse.json(header, { status: 200, statusText: 'header received successfully' });
+};
+
+export async function POST(request: NextRequest) {
+  const { title, param, public: isPublic } = await request.json() as Header;
+
+  const create = await prisma.header.create({
+    data: {
+      title,
+      param,
+      public: isPublic,
+    }
+  });
+
+  if (!create) {
+    return NextResponse.json([], { status: 400, statusText: 'header not received' });
+  };
+
+  return NextResponse.json(true, { status: 200, statusText: 'header received successfully' });
 };
