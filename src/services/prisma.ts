66731b5ed9fdacd8a689b434b440456ae92c4ae2
dyson-mori <161769883+dyson-mori@users.noreply.github.com>
2025-05-08 09:02:13 -0300

    fix production error

--- src/services/prisma.ts
+++ src/services/prisma.ts
@@ -1,5 +1,13 @@
 import { PrismaClient } from '@prisma/client'
 
-export const prisma = new PrismaClient({
-  log: ['info'],
-})
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined
+}
+
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: ['error'], // ou 'info', 'query' se quiser mais debug
+  })
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
