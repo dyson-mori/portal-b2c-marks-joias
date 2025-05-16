5749ab60b5ab14536ad4248cab5c155b69566d28
dyson-mori <161769883+dyson-mori@users.noreply.github.com>
2025-05-16 15:52:59 -0300

    ✨ adding thumbnail

--- src/app/success/app/index.tsx
+++ src/app/success/app/index.tsx
@@ -1,34 +1,45 @@
 "use client"
 
-// import Image from "next/image";
+import Image from "next/image";
 
-import { Container, Label } from "./styles";
+import { Container, Images, Label } from "./styles";
 
 type Props = {
   data: {
-    fullname: string;
-    products: {
-      image: string;
-    }[];
+    ip_address: "177.55.231.177",
+    items: [
+      {
+        description: "O Colar Pavão Real Multicolor é uma peça vibrante e cheia de personalidade. Com um pingente em formato de pavão, cravejado com zircônias coloridas que remetem à beleza das penas dessa ave majestosa, ele transmite elegância e originalidade. O corpo ",
+        id: "120005",
+        quantity: "1",
+        title: "Colar Pavão Real Multicolor",
+        thumbnail: string;
+        unit_price: "1"
+      }
+    ],
+    payer: {
+      first_name: "Sergio Junio Leal"
+    },
+    tracking_id: "platform:v1-whitelabel,so:ALL,type:N/A,security:none"
   }
 };
 
 export default function SuccessApp({ data }: Props) {
-  console.log(data);
   return (
     <Container>
-      {/* <Images>
-        {data.products.map((row, index) => (
+      <Images>
+        {data.items.map((row, index) => (
           <Image
             priority
             key={index.toString()}
             width={350}
             height={350}
-            src={row.image}
-            alt={row.image}
+            src="https://res.cloudinary.com/doo9pfft1/image/upload/v1747416129/120753_vzgnde.jpg"
+            // src={row.thumbnail}
+            alt={row.title}
           />
         ))}
-      </Images> */}
+      </Images>
 
       <Label>
         <h3>Obrigado pela Compra</h3>
