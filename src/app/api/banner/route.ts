b7ea2d37528a720dfd797d8282f0f81f00d86208
dyson-mori <161769883+dyson-mori@users.noreply.github.com>
2025-05-18 21:55:51 -0300

    ✨ banner crud

--- src/app/api/banner/route.ts
+++ src/app/api/banner/route.ts
@@ -2,14 +2,14 @@ import { NextRequest, NextResponse } from "next/server";
 
 import { prisma } from "@services/prisma";
 
-export async function GET(request: NextRequest) {
+export async function GET() {
   const data = await prisma.banner.findMany({
     where: {
       public: true
     }
   });
 
-  return NextResponse.json(data, { status: 200, statusText: 'products received successfully' });
+  return NextResponse.json(data, { status: 200, statusText: 'banner received successfully' });
 };
 
 export async function POST(request: NextRequest) {
@@ -19,5 +19,46 @@ export async function POST(request: NextRequest) {
     data: body
   });
 
-  return NextResponse.json(data, { status: 200, statusText: 'products received successfully' });
+  if (!data) {
+    return NextResponse.json(false, { status: 400, statusText: 'banner server error' });
+  };
+
+  return NextResponse.json(true, { status: 200, statusText: 'banner received successfully' });
+};
+
+export async function PUT(request: NextRequest) {
+  const url = new URL(request.url);
+  const banner_id = url.searchParams.get("banner_id") as string;
+
+  const body = await request.json();
+
+  const data = await prisma.banner.update({
+    where: {
+      id: banner_id
+    },
+    data: body
+  });
+
+  if (!data) {
+    return NextResponse.json(false, { status: 400, statusText: 'banner server error' });
+  };
+
+  return NextResponse.json(true, { status: 200, statusText: 'banner received successfully' });
+};
+
+export async function DELETE(request: NextRequest) {
+  const url = new URL(request.url);
+  const banner_id = url.searchParams.get("banner_id") as string;
+
+  const data = await prisma.banner.delete({
+    where: {
+      id: banner_id
+    }
+  });
+
+  if (!data) {
+    return NextResponse.json(false, { status: 400, statusText: 'banner server error' });
+  };
+
+  return NextResponse.json(true, { status: 200, statusText: 'banner received successfully' });
 };
