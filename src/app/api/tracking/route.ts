74d0352c83ee2be7a20f518de9b4ac9b11861a4a
dyson-mori <161769883+dyson-mori@users.noreply.github.com>
2025-05-23 13:16:12 -0300

    ✨ change search

--- src/app/api/tracking/route.ts
+++ src/app/api/tracking/route.ts
@@ -2,30 +2,15 @@ import { NextRequest, NextResponse } from "next/server";
 
 import { prisma } from "@services/prisma";
 
-export async function OPTIONS() {
-  return NextResponse.json({}, {
-    status: 200,
-    headers: {
-      'Access-Control-Allow-Origin': '*', // ou restrinja ao domínio ngrok
-      'Access-Control-Allow-Methods': 'POST, OPTIONS',
-      'Access-Control-Allow-Headers': 'Content-Type',
-    },
-  });
-};
-
 export async function GET(request: NextRequest) {
   const url = new URL(request.url);
   const order_id = url.searchParams.get("order_id") as string;
 
   const data = await prisma.order.findFirst({
     where: {
-      id: order_id
+      external_reference: order_id
     }
   });
 
-  return NextResponse.json(data, {
-    status: 200, statusText: 'products received successfully', headers: {
-      'Access-Control-Allow-Origin': '*', // ou use seu domínio ngrok
-    }
-  });
+  return NextResponse.json(data, { status: 200, statusText: 'products received successfully' });
 };
